b033f9ddede38ea34ecf6d77e21c3549479630ef
Nikola Lukic <nlukic97@gmail.com>
2021-11-05 13:21:23 +0100

    game works with console logging the result

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -19,31 +19,55 @@ let points = [
     }
 ]
 
-let player1 = 1
-let player2 = 2
-
 function getTitle(id){
     // the id is the same as the game title (0. rock, 1. paper, 2. scissors)
     return ['rock','paper','scissors'][id]
 }
 
 // Check if player 1 has won
-function checkResult(player1, player2){
+function getPoints(player1, player2){
+
     for(let i = 0; i < points.length; i++){
         if(points[i].title === player1){
             return points[i][player2]
         }
+    }    
+}
+
+function checkResult(outcome){
+    switch(outcome){
+        case -1:
+        console.log('You lose');
+        break;
+        case 0:
+        console.log('Tie');
+        break;
+        case 1:
+        console.log('You win');
+        break;
     }
 }
 
-switch(checkResult( getTitle(player1) , getTitle(player2) )){
-    case -1:
-        console.log('You lose');
-        break;
-    case 0:
-        console.log('Tie');
-        break;
-    case 1:
-        console.log('You win');
-        break;
+function getRandomInt(){
+    return Math.floor(Math.random() * 3); //0, 1, or 2
 }
+
+
+
+document.querySelector('#paper').addEventListener('click',function(){
+    let computerChoice = getRandomInt()
+    let outcome = getPoints(getTitle(this.getAttribute('data-id')),getTitle(computerChoice))
+    checkResult(outcome)
+})
+
+document.querySelector('#scissors').addEventListener('click',function(){
+    let computerChoice = getRandomInt()
+    let outcome = getPoints(getTitle(this.getAttribute('data-id')),getTitle(computerChoice))
+    checkResult(outcome)
+})
+
+document.querySelector('#rock').addEventListener('click',function(){
+    let computerChoice = getRandomInt()
+    let outcome = getPoints(getTitle(this.getAttribute('data-id')),getTitle(computerChoice))
+    checkResult(outcome)
+})
